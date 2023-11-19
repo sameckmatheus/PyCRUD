c969b7f645b143dfa0ff91b4a8c692b7678bc46f
sameckmatheus <sameckmatheuspro@gmail.com>
2023-11-19 13:12:39 -0300

    feat: remove bugs / chore: add main.py caontent

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+from dml_lib import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+conexao = ConexaoBanco()
+operacoes = OperacoesBanco(conexao.conexao, conexao.cursor)
 
+while True:
+    exibir_menu()
+    opcao = input("Digite o número da opção desejada (1-5): ")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    if opcao == "1":  # DELETE
+        matricula_aluno_delete = input("Digite a matricula do aluno a ser removido: ")
+        comando_delete = f'DELETE FROM albinos WHERE matricula = "{matricula_aluno_delete}"'
+        operacoes.executar_comando(comando_delete)
 
+    elif opcao == "2":  # CREATE
+        nome_aluno_create = input("Digite o nome do novo aluno: ")
+        matricula_create = input("Digite a matricula do novo aluno: ")
+        cpf_create = input("Digite o CPF do novo aluno: ")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        comando_create = (f'INSERT INTO alunos (nome, matricula, cpf) VALUES ('
+                          f'"{nome_aluno_create}", '
+                          f'{matricula_create}, '
+                          f'"{cpf_create}")')
+        operacoes.executar_comando(comando_create)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    elif opcao == "3":  # READ
+        comando_read = f'SELECT * FROM alunos'
+        resultado_read = operacoes.buscar_dados(comando_read)
+        print(resultado_read)
+
+    elif opcao == "4":  # UPDATE
+        nome_aluno_update = input("Digite o nome do produto a ser atualizado: ")
+        matricula_update = input("Digite a nova matricula do aluno: ")
+        comando_update = f'UPDATE alunos SET matricula = {matricula_update} WHERE nome = "{nome_aluno_update}"'
+        operacoes.executar_comando(comando_update)
+
+    elif opcao == "5":  # Sair
+        break
+
+    else:
+        print("pção inválida. Por favor, escolha uma opção válida.")
+        continue
+
+conexao.fechar_conexao()
